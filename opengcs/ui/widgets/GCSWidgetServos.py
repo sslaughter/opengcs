59d94473a84e798e4d002354b40d47da732c0839
Scott Slaughter <scott.slaughter@yahoo.com>
2015-11-01 20:14:18 -0800

    Refine grid, add import into mainwindow.py to allow using the servos widget, add to default perspective to make it show up by default

--- opengcs/ui/widgets/GCSWidgetServos.py
+++ opengcs/ui/widgets/GCSWidgetServos.py
@@ -1,6 +1,7 @@
 from GCSWidget import *
 from PyQt4.QtGui import *
-from PyQt4 import QtCore
+from PyQt4.QtCore import *
+import sys
 from pymavlink import mavutil
 
 
@@ -57,28 +58,44 @@ class GCSWidgetServos (GCSWidget):
 
     def init_ui(self):
 
+        mylayout = QWidget()
         servo_grid = QGridLayout()
-        self.setLayout(servo_grid)
+        #servo_grid.setHorizontalSpacing(1)
+        mylayout.setLayout(servo_grid)
 
+        self.setWidget(mylayout)
+        self.setWindowTitle("Mav Servos")
 
         for servo_num in range (0, self.numServos):
 
-            servo_name = QLabel(servo_num, self)
-            servo_grid.addWidget(servo_name, 0, servo_num)
+            servo_name = QLabel(QString.number(servo_num + 5), self)
+            servo_name.setAlignment(Qt.AlignCenter)
+            servo_grid.addWidget(servo_name, servo_num, 0)
 
-            servo_Min = QPushButton("Set Low: %d" % (self.focused_object.mav_param['RC%d_MIN' % (servo_num+5)]))
-            servo_Min.clicked().connect(self.set_Servo(servo_num + 5,self.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
-            servo_grid.addWidget(servo_Min, 1, servo_num)
+            try:
+                self.state.focused_object.mav_param
+                servo_Min = QPushButton("Set Low: %d" % (QString.number(self.state.focused_object.mav_param['RC%d_MIN' % (servo_num+5)])))
+                servo_Min.clicked.connect(self.set_Servo(servo_num + 5,self.state.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
+            except:
+                servo_Min = QPushButton("Set Low: NO MAV!")
 
-            servo_Max = QPushButton("Set High: %d" % (self.focused_object.mav_param['RC%d_MAX' % (servo_num+5)]))
-            servo_Max.clicked().connect(self.set_Servo(servo_num + 5,self.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
-            servo_grid.addWidget(servo_Max, 2, servo_num)
+            servo_grid.addWidget(servo_Min, servo_num, 1)
+
+            try:
+                self.state.focused_object.mav_param
+                servo_Max = QPushButton("Set High: %d" % (QString.number(self.state.focused_object.mav_param['RC%d_MAX' % (servo_num+5)])))
+                servo_Max.clicked.connect(self.set_Servo(servo_num + 5,self.state.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
+            except:
+                servo_Max = QPushButton("Set High: NO MAV!")
+
+            servo_grid.addWidget(servo_Max, servo_num, 2)
 
             servo_Current = QLabel("Waiting", self)
-            servo_grid.addWidget(servo_Current, 3, servo_num)
+            servo_grid.addWidget(servo_Current, servo_num, 3)
 
 
-        self.refresh()
+       # self.refresh()
+        #self.show()
 
     def refresh(self):
 
