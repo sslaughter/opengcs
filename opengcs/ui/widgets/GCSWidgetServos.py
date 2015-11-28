f2dd301192eac7e0ae25c8723f70e3f3914a8dee
Scott Slaughter <scott.slaughter@yahoo.com>
2015-11-28 12:10:34 -0800

    continue master rebase

--- opengcs/ui/widgets/GCSWidgetServos.py
+++ opengcs/ui/widgets/GCSWidgetServos.py
@@ -1,8 +1,12 @@
 from GCSWidget import *
 from PyQt4.QtGui import *
 <<<<<<< HEAD
+<<<<<<< HEAD
 from PyQt4.QtCore import *
 import sys
+=======
+from PyQt4 import QtCore
+>>>>>>> Initial layout attempt, empty set servo method
 from pymavlink import mavutil
 
 
@@ -44,10 +48,13 @@ Only needs to affect servo values for a particulare vehicle, doesn't need any kn
 
 """
 
+<<<<<<< HEAD
 =======
 
 
 >>>>>>> Initial commit for servos widget file
+=======
+>>>>>>> Initial layout attempt, empty set servo method
 
 
 class GCSWidgetServos (GCSWidget):
@@ -59,13 +66,18 @@ class GCSWidgetServos (GCSWidget):
         self.setObjectName("GCSWidgetServos")
         self.set_datasource_allowable(WidgetDataSource.SINGLE)
 <<<<<<< HEAD
+<<<<<<< HEAD
         self.numServos = 7
 =======
 >>>>>>> Initial commit for servos widget file
+=======
+        self.numServos = 7
+>>>>>>> Initial layout attempt, empty set servo method
         self.init_ui()
 
     def init_ui(self):
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         mylayout = QWidget()
         servo_grid = QGridLayout()
@@ -111,12 +123,36 @@ class GCSWidgetServos (GCSWidget):
         self.label_throttle = QLabel("0", self)
         self.label_heading = QLabel("0", self)
         self.label_climbrate = QLabel("0", self)
+=======
+        servo_grid = QGridLayout()
+        self.setLayout(servo_grid)
+
+
+        for servo_num in range (0, self.numServos):
+
+            servo_name = QLabel(servo_num, self)
+            servo_grid.addWidget(servo_name, 0, servo_num)
+
+            servo_Min = QPushButton("Set Low: %d" % (self.focused_object.mav_param['RC%d_MIN' % (servo_num+5)]))
+            servo_Min.clicked().connect(self.set_Servo(servo_num + 5,self.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
+            servo_grid.addWidget(servo_Min, 1, servo_num)
+
+            servo_Max = QPushButton("Set High: %d" % (self.focused_object.mav_param['RC%d_MAX' % (servo_num+5)]))
+            servo_Max.clicked().connect(self.set_Servo(servo_num + 5,self.focused_object.mav_param['RC%d_Min' % (servo_num+5)]))
+            servo_grid.addWidget(servo_Max, 2, servo_num)
+
+            servo_Current = QLabel("Waiting", self)
+            servo_grid.addWidget(servo_Current, 3, servo_num)
+
+
+>>>>>>> Initial layout attempt, empty set servo method
         self.refresh()
 >>>>>>> Initial commit for servos widget file
 
     def refresh(self):
 
         super(GCSWidgetServos, self).refresh()
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 
@@ -128,13 +164,18 @@ class GCSWidgetServos (GCSWidget):
 =======
         w = self.geometry().width()
         h = self.geometry().height()
+=======
+>>>>>>> Initial layout attempt, empty set servo method
 
-        self.label_altitude.move(5, h/2-10)
-        self.label_airspeed.move(w-25, h/2-10)
-        self.label_throttle.move(5, 20)
-        self.label_heading.move(w/2 - 10, 50)
-        self.label_climbrate.move(w-25,20)
 
+<<<<<<< HEAD
         self.setWindowTitle("GCSWidgetServos")
         self.setMinimumSize(200, 200)
 >>>>>>> Initial commit for servos widget file
+=======
+    def set_Servo(self, servo, value):
+
+        # set servo value
+
+        print("Setting Servo: %d, to value: %d" % (servo, value))
+>>>>>>> Initial layout attempt, empty set servo method
